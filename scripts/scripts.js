b050469e3fa0bb1a7c59a3e89e08d6686b3bf7a4
payalshah30 <spayal1130@gmail.com>
2026-04-24 15:15:58 +0530

    update file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -187,11 +187,7 @@ async function loadPage() {
 }
 
 loadPage();
-
 (async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
-
-  // eslint-disable-next-line import/no-unresolved
-  import('https://da.live/scripts/dapreview.js')
-    .then(({ default: daPreview }) => daPreview(loadPage));
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
