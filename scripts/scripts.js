9b97a7f8139e5c9b8c30f7e9d4cea30e91c81292
payalshah30 <spayal1130@gmail.com>
2026-04-24 15:02:21 +0530

    updated file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -191,4 +191,4 @@ loadPage();
 (async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
-}());
+}());  
