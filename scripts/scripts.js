f2e5b67b58880364c248318306b8974dcaf474bf
payalshah30 <spayal1130@gmail.com>
2026-04-24 16:28:19 +0530

    chnges added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -189,6 +189,5 @@ async function loadPage() {
 loadPage();
 
 (async function loadDa() {
-  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
-  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return; import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
