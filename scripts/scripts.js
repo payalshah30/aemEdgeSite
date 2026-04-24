4e4086d7899105225e937ac77670b75552ac8645
payalshah30 <spayal1130@gmail.com>
2026-04-24 16:09:05 +0530

    changes added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -192,4 +192,3 @@ loadPage();
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
-
